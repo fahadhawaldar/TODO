83474692584a4b2d94efd328dd38702edf7320c8
fahadhawaldar <fahadhawaldar@gmail.com>
2025-03-28 15:49:43 +0530

    Release

---
+++ android/app/build/intermediates/cxx/RelWithDebInfo/5v2e46q1/logs/x86_64/build_command_targets.bat
@@ -0,0 +1,7 @@
+@echo off
+"C:\\Users\\fahad\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "G:\\React Native\\TODO\\Expo todo\\TODO\\android\\app\\.cxx\\RelWithDebInfo\\5v2e46q1\\x86_64" ^
+  appmodules ^
+  react_codegen_rnscreens ^
+  react_codegen_safeareacontext
