3affc910337657119731e0a0351e77dbfe752db2
fahadhawaldar <fahadhawaldar@gmail.com>
2025-03-28 16:08:08 +0530

    Remove android folder from repository

--- android/app/.cxx/RelWithDebInfo/5v2e46q1/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "C:/Users/fahad/.gradle/caches/8.10.2/transforms/14479106fc9c285034fe5188965917a6/transformed/fbjni-0.6.0/prefab/modules/fbjni/libs/android.armeabi-v7a/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/fahad/.gradle/caches/8.10.2/transforms/14479106fc9c285034fe5188965917a6/transformed/fbjni-0.6.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/RelWithDebInfo/5v2e46q1/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/fahad/.gradle/caches/8.10.2/transforms/de6fa714924ea18db1a35977514a2c3a/transformed/hermes-android-0.76.7-release/prefab/modules/libhermes/libs/android.x86/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/fahad/.gradle/caches/8.10.2/transforms/de6fa714924ea18db1a35977514a2c3a/transformed/hermes-android-0.76.7-release/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
